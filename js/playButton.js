8b303a94106c58403db0fda6a7e5a5e9241bdbc1
8-Bit Hive Technology - Nick Collins <nick@8bithive.com>
2024-12-17 10:04:58 -0500

    Video Update

--- js/playButton.js
+++ js/playButton.js
@@ -4,7 +4,7 @@ document.getElementById("play").addEventListener("click", function() {
 
     // Get the YouTube iframe and set its source with autoplay enabled
     var youtubeVideo = document.getElementById("youtube-video");
-    youtubeVideo.src = "https://www.youtube.com/embed/QdBZY2fkU-0?autoplay=1";  // Note the autoplay=1 in the URL
+    youtubeVideo.src = "https://www.youtube.com/embed/CeBb40wcOxw?autoplay=1"; // Correct format
 
     // Display the video container
     document.getElementById("responsive-video").style.display = "block";
