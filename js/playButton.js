7c0b0b4e4db82c230ed0cd539296c917aa5e9cf1
8-Bit Hive Technology - Nick Collins <nick@8bithive.com>
2025-01-06 15:36:45 -0500

    YouTube Ending Fix

--- js/playButton.js
+++ js/playButton.js
@@ -4,7 +4,7 @@ document.getElementById("play").addEventListener("click", function() {
 
     // Get the YouTube iframe and set its source with autoplay enabled
     var youtubeVideo = document.getElementById("youtube-video");
-    youtubeVideo.src = "https://www.youtube.com/embed/CeBb40wcOxw?autoplay=1"; // Correct format
+    youtubeVideo.src = "https://www.youtube.com/embed/CeBb40wcOxw?autoplay=1&rel=0"; // Correct format
 
     // Display the video container
     document.getElementById("responsive-video").style.display = "block";
